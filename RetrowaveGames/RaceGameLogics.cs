5c9fc7007c2f3f9768459630a705a5115ef04df9
Your Name <you@example.com>
2020-12-03 17:14:06 +0500

    продолжаю писать логику

--- RetrowaveGames/RaceGameLogics.cs
+++ RetrowaveGames/RaceGameLogics.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml.Serialization;
+﻿using System;
+using System.Xml.Serialization;
 
 namespace RetrowaveGames
 {
@@ -22,6 +23,8 @@ namespace RetrowaveGames
 
         private static int FieldWidth = 10, FieldHeight = 20;
         private static int CurrentPlayerLine = 0;
+        private static int TicksFromPrevGeneration = 0, TicksToNextGeneration = 0, MinTicksToGenerate = 5;
+        private static int[,] HiddenArea = new int[FieldWidth, 4];
         private static int[,] GameField = new int[FieldWidth, FieldHeight];
 
         private static void ResetGame()
@@ -34,6 +37,13 @@ namespace RetrowaveGames
                 }
             }
 
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    HiddenArea[i, j] = 0;
+                }
+            }
             CurrentPlayerLine = 0;
         }
 
@@ -49,6 +59,80 @@ namespace RetrowaveGames
             if (CurrentPlayerLine == 1)
                 return;
             CurrentPlayerLine = 1;
+        }
+
+        public static void TimerTick()
+        {
+            GenerateEnemy();
+
+            for (int i = 0; i < FieldHeight; i++)
+            {
+
+            }
+        }
+
+        private static void GenerateEnemy()
+        {
+            if (TicksFromPrevGeneration < TicksToNextGeneration)
+            {
+                TicksFromPrevGeneration++;
+            }
+            else
+            {
+                TicksFromPrevGeneration = 0;
+                Random rand_gen = new Random();
+                TicksToNextGeneration = MinTicksToGenerate + rand_gen.Next(0, 6);
+                int enemypos = rand_gen.Next(0, 2), delta = 0;
+                switch (enemypos)
+                {
+                    case 0:
+                        delta = 2;
+                    break;
+                    case 1:
+                        delta = 5;
+                    break;
+                }
+
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        HiddenArea[i + delta, j] = EnemyCar[i, j];
+                    }
+                }
+            }
+        }
+
+        public static bool GetPixel(int x, int y)
+        {
+            int[,] LocalCopy = new int[FieldWidth, FieldHeight];
+            for (int i = 0; i < FieldWidth; i++)
+            {
+                for (int j = 0; j < FieldHeight; j++)
+                {
+                    LocalCopy[i, j] = GameField[i, j];
+                }
+            }
+
+            int Delta = 0;
+            switch (CurrentPlayerLine)
+            {
+                case 0:
+                    Delta = 2;
+                break;
+                case 1:
+                    Delta = 5;
+                break;
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    LocalCopy[i + Delta, j + 15] = PlayerCar[i, j];
+                }
+            }
+            return true;
         }
     }
 }
